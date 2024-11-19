8ddaf73906771471abc89c385a93c40797c0254f
ademulic <ademulic@hotmail.com>
2024-11-19 20:15:59 +0100

    UREDJEN PROJEKAT

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,29 @@
 import axios from "axios";
-import { useEffect, useState } from "react";  
+import { useEffect, useState } from "react";   
 import Card from "../Card/Card";
 import "./Home.css";
 import Error from "../Error/Error";
-const Home = () => {
 
+const Home = () => {
     const [news,setNews] = useState([]);
-    let [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [error, setError]= useState(null);
+    
     useEffect(()=>{
-        axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2024-10-14&sortBy=publishedAt&apiKey=56500b46b329476f87fb809ba3374370`
-        ).then(data=>{
-            setNews(data.data.articles); 
+        axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2024-10-16&sortBy=publishedAt&apiKey=56500b46b329476f87fb809ba3374370
+        `)
+        .then(data=>{
+            
+            setNews(data.data.articles.filter(article=>article.title!=='[Removed]')); 
             setLoading(false);
             setError(null);
+
         }).catch(err=>{
+            
             setNews([]);
             setError(err);
             setLoading(false);
+       
         }); 
 
     
@@ -29,11 +35,10 @@ const Home = () => {
           {!error && <h1>TOP NEWS</h1>}
           {error && <Error/>}
           <section className="main-news-section">
-          
-            
+        
             {loading && <p>Loading...</p>}
-            {news && news.map((newsArticle,index)=>(
-                <Card newsArticle = {newsArticle} index={index}/> 
+            {news.length && news.map((newsArticle,index)=>(
+                <Card newsArticle = {newsArticle} index={index} key={index}/> 
               ))
             } 
           </section>
